186d4f311162e640dda9c038f3181da06d1b2582
Ben Wasserman <bwasserm@andrew.cmu.edu>
2013-01-01 17:30:29 -0500

    Added Atmel Studio 6 project for code to run on ATTiny25.

---
+++ ATTiny25/ADC_RMS/ADC_RMS/ADC_RMS.c
@@ -0,0 +1,28 @@
+/*
+ * ADC_RMS.c
+ *
+ * Created: 1/1/2013 15:36:32 PM
+ *  Author: bwasserm
+ */ 
+
+
+#include <avr/io.h>
+#include "TWI_Slave.h"
+
+int main(void)
+{
+	// Setup AVR
+	
+	// Setup ADC
+	
+	// Setup I2C
+	
+    while(1)
+    {
+        // Read ADC
+		
+		// Calculate RMS
+		
+		// Transmit Data
+    }
+}
