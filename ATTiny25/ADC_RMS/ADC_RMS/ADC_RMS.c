b1c8dd990432200e8d05788221ba50b2fa57a955
Ben Wasserman <bwasserm@andrew.cmu.edu>
2013-01-03 13:17:38 -0500

    Added in ADC code to setup and read from the ADC in the loop. Once this works, we can convert it to an interrupt driven system.

--- ATTiny25/ADC_RMS/ADC_RMS/ADC_RMS.c
+++ ATTiny25/ADC_RMS/ADC_RMS/ADC_RMS.c
@@ -15,24 +15,52 @@ int main(void)
 {
 	// Setup variables
 	uint8_t count = 0;
+	int8_t adc_val = 0;
 	
 	// Setup AVR
 	
 	// Setup ADC
+	// Set ADC Mux
+	ADMUX = (0<<REFS2)|(0<<REFS1)|(0<<REFS0)|	// Use VCC as Vref, disconnected from AREF pin (which is in use as SDA for TWI)
+			(1<<ADLAR)|							// Left-shift the ADC result, so the first 8 bits can be read from a single register (ADCH)
+			(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0);	// Use difference between PB4 and PB3, with gain of 1x. Use 0111 for 20x gain
+	// Set ADC Control Register A
+	ADCSRA = (1<<ADEN)|							// Enable ADC
+			(0<<ADSC)|							// Don't start conversion yet
+			(0<<ADATE)|							// Don't run on triggering
+			(0<<ADIF)|(0<<ADIE)|				// Don't use the ADC interrupt
+			(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0);	// Set the clock prescaler to 64, so the 8MHz CPU clock gets scaled to 125kHz
+	// Set ADC Control Register B
+	ADCSRB |= (1<<BIN)|							// Enable bipolar input mode, so we can measure positive and negative voltages (for AC)
+			(0<<IPR)|							// Don't enable polarity reversal. It doesn't do us any good
+			(0<<ADTS2)|(0<<ADTS1)|(0<<ADTS0);	// Set ADC to free running mode, so it continually samples
+	// Disable digital pins being used by ADC
+	DIDR0 |= (1<<ADC3D)|(1<<ADC3D)|				// Disable digital pins being used by ADC
+			(0<<ADC3D)|(0<<ADC3D);				// Leave enabled pins being used for digital
 	
-	// Setup I2C
+	// Setup I2C (TWI)
 	usiTwiSlaveInit( (uint8_t)TWI_ADDRESS );
+	
+	// Start ADC conversion
+	ADCSRA |= (1<<ADSC);
 	
     while(1)
     {
         // Read ADC
+		adc_val = ADCH;
 		
 		// Calculate RMS
 		
 		// Transmit Data
 		if(usiTwiDataInTransmitBuffer() == false){
+			// Transmit count for testing
 			usiTwiTransmitByte(count);
 			count++;
-		}			
+		}
+		if(usiTwiDataInTransmitBuffer() == false){
+			// Transmit ADC value
+			usiTwiTransmitByte((uint8_t)adc_val);
+			count++;
+		}		
     }
 }
