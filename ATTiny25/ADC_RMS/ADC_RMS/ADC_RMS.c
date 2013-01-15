a42bda0264fa38c6f9f871df6a703244c7098b65
Ben Wasserman <bwasserm@andrew.cmu.edu>
2013-01-14 20:18:54 -0500

    Got TWI working and ADC working.

--- ATTiny25/ADC_RMS/ADC_RMS/ADC_RMS.c
+++ ATTiny25/ADC_RMS/ADC_RMS/ADC_RMS.c
@@ -10,7 +10,7 @@
 #include <avr/interrupt.h>
 #include "usiTwiSlave.h"
 
-#define TWI_ADDRESS 42
+#define TWI_ADDRESS 2
 #define LED_TOGGLE() (PINB |= (1<<PINB1))
 
 int main(void)
@@ -27,11 +27,12 @@ int main(void)
 	// Set ADC Mux
 	ADMUX = (0<<REFS2)|(0<<REFS1)|(0<<REFS0)|	// Use VCC as Vref, disconnected from AREF pin (which is in use as SDA for TWI)
 			(1<<ADLAR)|							// Left-shift the ADC result, so the first 8 bits can be read from a single register (ADCH)
-			(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0);	// Use difference between PB4 and PB3, with gain of 1x. Use 0111 for 20x gain
+//			(0<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0);	// Use difference between PB4 and PB3, with gain of 1x. Use 0111 for 20x gain
+			(0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(0<<MUX0);	// Read ADC2 (PB4)
 	// Set ADC Control Register A
 	ADCSRA = (1<<ADEN)|							// Enable ADC
 			(0<<ADSC)|							// Don't start conversion yet
-			(1<<ADATE)|							// Enable triggering so Free Running mode can be set
+			(0<<ADATE)|							// Disable triggering //so Free Running mode can be set
 			(0<<ADIF)|(0<<ADIE)|				// Don't use the ADC interrupt
 			(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0);	// Set the clock prescaler to 64, so the 8MHz CPU clock gets scaled to 125kHz
 	// Set ADC Control Register B
@@ -54,7 +55,7 @@ int main(void)
     {
         // Read ADC
 		adc_val = ADCH;
-//		ADCSRA = ADCSRA | (1<<ADSC);		// Reinable for next conversion to happen
+		ADCSRA = ADCSRA | (1<<ADSC);		// Reinable for next conversion to happen
 		
 		// Calculate RMS
 		
@@ -62,7 +63,7 @@ int main(void)
 		// Transmit Data
 		if(usiTwiDataInTransmitBuffer() == false){
 			// Transmit count for testing
-			usiTwiTransmitByte(count);
+			//usiTwiTransmitByte(count);
 			count++;
 		}
 		if(usiTwiDataInTransmitBuffer() == false){
@@ -72,7 +73,7 @@ int main(void)
 		}
 		
 		// Toggle LED heartbeat
-		if(adc_val < 0){
+		if(adc_val < 64){
 			if(led_counter++ == 0){
 				LED_TOGGLE();
 			}
