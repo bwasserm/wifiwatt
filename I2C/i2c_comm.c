9576da1bcb081578167889cc282e80237e326a79
bwasserm <benjamin@cmu.edu>
2013-01-15 08:12:18 +0000

    Added in code to do syncronization on I2C. It doesn't work right though. Still, commiting just in case.

--- I2C/i2c_comm.c
+++ I2C/i2c_comm.c
@@ -20,6 +20,9 @@
 // Address of device
 #define ADDRESS 0x2a
 
+// I2C sync constants
+#define ZERO_MASK 0xFC
+
 // Open fd and set up as i2c (-1 on error)
 int init_ADC(void){
     int fd;
@@ -39,7 +42,7 @@ int init_ADC(void){
 
 // Get the 16 bit value from the ADC (-1 on error)
 int16_t get_ADC(int fd){
-    char buf[4] = {0};
+    char buf[2] = {0};
 
     // Using I2C Read
     if(read(fd, buf, 2) != 2) {
@@ -49,8 +52,20 @@ int16_t get_ADC(int fd){
     return (int16_t)(((buf[0] & 0b0011) << 8) | buf[1]);
 }
 
+// Get a 1 byte value from the ADC
+uint8_t get_ADC_byte(int fd){
+    uint8_t byte = 0;
+
+    // Using I2C read
+    read(fd, &byte, 1);
+
+    return byte;
+}
+
 int main(){
     int16_t data;
+    uint8_t h_byte = 0;
+    uint8_t l_byte = 0;
     int fd;
 
     if((fd = init_ADC()) < 0){
@@ -58,10 +73,37 @@ int main(){
     }
 
     while(1){
-        if((data = get_ADC(fd)) < 0)
+      // Read in 2 bytes
+      data = get_ADC(fd);
+
+      // Check for sync errors
+      h_byte = (uint8_t)(data >> 8);
+      l_byte = (uint8_t)(data);
+
+      if((h_byte & ZERO_MASK) != 0){
+        // The first byte shouldn't be a high byte.
+        if((l_byte & ZERO_MASK) != 0){
+          // There is most likely a missing byte. Throw out current data
+          printf("Sync error: can't recover. Resyncing...\n");
+          continue;
+        }
+        else{
+          // It is possible that the second byte is supposed to be a high byte.
+          // Try resyncing by reading one more byte
+          printf("Sync error: resyncing...\n");
+          h_byte = l_byte;
+          l_byte = get_ADC_byte(fd);
+        }
+      }
+      printf("Data: %d\n", data);
+
+/*
+** The sync code doesn't support error codes. The AVR doesn't send them anyways
+      if((data = get_ADC(fd)) < 0)
             printf("Failed to read data: %s\n", strerror(errno));
         else
             printf("Data: %d\n", data);
+*/
     }
 
     if(close(fd) < 0)
