e7a1f1f77cdb8a0d513cec51ef14376da9bb99e0
bwasserm <benjamin@cmu.edu>
2013-01-16 01:24:07 +0000

    Started test file for GPIO (Neil working as Ben)

---
+++ I2C/gpio_test.c
@@ -0,0 +1,115 @@
+/* gpio_test.c
+** Tests GPIO functionality on the Rapsberry Pi
+*/
+#define BCM2708_PERI_BASE        0x20000000
+#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define PAGE_SIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+int  mem_fd;
+char *gpio_map;
+
+// I/O access
+volatile unsigned *gpio;
+
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)  // sets bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+#define RELAY_NUM 14
+
+void setup_io();
+
+int main(int argc, char **argv)
+{
+  int g,rep;
+
+  // Set up gpi pointer for direct register access
+  setup_io();
+
+  // Switch GPIO 7..11 to output mode
+  
+  /************************************************************************\
+   * You are about to change the GPIO settings of your computer.          *
+   * Mess this up and it will stop working!                               *
+   * It might be a good idea to 'sync' before running this program        *
+   * so at least you still have your code changes written to the SD-card! * \************************************************************************/
+
+  INP_GPIO(RELAY_NUM);
+  OUT_GPIO(RELAY_NUM);
+  for(rep=0; rep<100; )
+  {
+    GPIO_SET = 1 << RELAY_NUM;
+    sleep(1);
+    GPIO_CLR = 1 << RELAY_NUM;
+    sleep(1);
+  }
+  return 0;
+/*
+  // Set GPIO pins 7-11 to output
+  for (g=7; g<=11; g++)
+  {
+    INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
+    OUT_GPIO(g);
+  }
+  for (rep=0; rep<10; rep++)
+  {
+    for (g=7; g<=11; g++)
+    {
+      GPIO_SET = 1<<g;
+      sleep(1);
+    }
+    for (g=7;  g<=11; g++)
+    {
+      GPIO_CLR = 1<<g;
+      sleep(1);
+    }
+  }
+  return 0;
+}
+*/
+
+//
+// Set up memory regions to access GPIO
+//
+void setup_io()
+{
+  /* open /dev/mem */
+  if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
+  {
+    printf("can't open /dev/mem \n");
+    exit(-1);
+  }
+
+  /* mmap GPIO */
+  gpio_map = (char *)mmap(
+      NULL,             //Any adddress in our space will do
+      BLOCK_SIZE,       //Map length
+      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+      MAP_SHARED,       //Shared with other processes
+      mem_fd,           //File to map
+      GPIO_BASE         //Offset to GPIO peripheral
+      );
+
+  close(mem_fd); //No need to keep mem_fd open after mmap
+
+  if ((long)gpio_map < 0) {
+    printf("mmap error %d\n", (int)gpio_map);
+    exit(-1);
+  }
+
+  // Always use volatile pointer!
+  gpio = (volatile unsigned *)gpio_map;    
+} // setup_io
