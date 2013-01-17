74e562bde5d5604b5047a76c4ba2ec916112f9f9
applepi <pi@applepi.andrew.cmu.edu>
2013-01-17 23:19:29 +0000

    Changes to make more rubust

--- AMQP_pi/amqp_wifiwatt.c
+++ AMQP_pi/amqp_wifiwatt.c
@@ -42,6 +42,8 @@ amqp_bytes_t route_cmd_name;
 ssize_t get_body(amqp_connection_state_t conn, char* buf, size_t buf_len);
 int perform_handshake(amqp_connection_state_t conn);
 amqp_connection_state_t connect_server(void);
+void report_error(int x, char const *context);
+void report_amqp_error(amqp_rpc_reply_t x, char const *context);
 
 int main(void) {
   char buf[BUF_LEN];
@@ -58,64 +60,78 @@ int main(void) {
   route_hand_name = amqp_cstring_bytes(ROUTE_HAND_NAME);
   route_cmd_name = amqp_cstring_bytes(ROUTE_CMD_NAME);
 
-  conn = connect_server();
-  printf("Finish connect\n");
-  perform_handshake(conn);
-  printf("Finish handshake\n");
-
-  if(fork() == 0){  // Child process
-    uint8_t val;
-    double current;
-
-    while(1){
-      //val = get_adc();
-      val = rand() % 256;
-      current = ((double) val / ADC_MAX * ADC_VOLT) / ADC_RES;
-      sprintf(buf, "%f", current);
-
-      // Publish values to server
-      die_on_error(amqp_basic_publish(conn, 1, exch_value_name, 
-        amqp_empty_bytes, 0, 0, NULL, amqp_cstring_bytes(buf)), "Publishing");
-
-      microsleep(2000);
+  while(1){
+    if((conn = connect_server()) == NULL){
+      continue;
     }
-  }
 
-  if(fork() == 0){  // Child process
-    // Consume cmd queue
-    amqp_basic_consume(conn, 1, queue_cmd_name, amqp_empty_bytes, 0, 1, 0, 
-      amqp_empty_table);
-    die_on_amqp_error(amqp_get_rpc_reply(conn), "consuming cmd queue");
+    printf("Finish connect\n");
 
-    while(1){
-      amqp_frame_t frame;
+    if(!perform_handshake(conn)){
+      continue;
+    }
+    printf("Finish handshake\n");
 
-      die_on_error(amqp_simple_wait_frame(conn, &frame), 
-        "waiting for header frame");
-
-      if(frame.frame_type != AMQP_FRAME_METHOD || 
-          frame.payload.method.id != AMQP_BASIC_DELIVER_METHOD)
-        continue;
-
-      if((mes_len = get_body(conn, buf, BUF_LEN)) < 0){
+    if(fork() == 0){  // Child process
+      uint8_t val;
+      double current;
+  
+      while(1){
+        //val = get_adc();
+        val = rand() % 256;
+        current = ((double) val / ADC_MAX * ADC_VOLT) / ADC_RES;
+        sprintf(buf, "%f", current);
+  
+        // Publish values to server
+        if(!report_error(amqp_basic_publish(conn, 1, exch_value_name, 
+            amqp_empty_bytes, 0, 0, NULL, amqp_cstring_bytes(buf)), "Publishing")){
+          exit(1);
+        }
+  
+        microsleep(2000);
+      }
+    }
+  
+    if(fork() == 0){  // Child process
+      // Consume cmd queue
+      amqp_basic_consume(conn, 1, queue_cmd_name, amqp_empty_bytes, 0, 1, 0, 
+        amqp_empty_table);
+      if(!report_amqp_error(amqp_get_rpc_reply(conn), "consuming cmd queue")){
         exit(1);
       }
-
-      // TODO implement command
-      printf("Command: %.*s\n", mes_len, buf);
-
-      amqp_maybe_release_buffers(conn);
+  
+      while(1){
+        amqp_frame_t frame;
+  
+        if(!report_error(amqp_simple_wait_frame(conn, &frame), 
+            "waiting for header frame")){
+          exit(1);
+        }
+  
+        if(frame.frame_type != AMQP_FRAME_METHOD || 
+            frame.payload.method.id != AMQP_BASIC_DELIVER_METHOD)
+          continue;
+  
+        if((mes_len = get_body(conn, buf, BUF_LEN)) < 0){
+          exit(1);
+        }
+  
+        // TODO implement command
+        printf("Command: %.*s\n", mes_len, buf);
+  
+        amqp_maybe_release_buffers(conn);
+      }
     }
+  
+    //TODO implement wait
+    while(1)
+      ;
+  
   }
-
-  while(1)
-    ;
 
   die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
   die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS), "Closing connection");
   die_on_error(amqp_destroy_connection(conn), "Ending connection");
-
-  return 0;
 }
 
 
@@ -125,44 +141,63 @@ amqp_connection_state_t connect_server(){
   amqp_connection_state_t conn = amqp_new_connection();
 
   // Open socket and set fd
-  die_on_error(sockfd = amqp_open_socket(HOSTNAME, PORT), "Opening socket");
+  if(!report_error(sockfd = amqp_open_socket(HOSTNAME, PORT), "Opening socket")){
+    return NULL;
+  }
   amqp_set_sockfd(conn, sockfd);
 
   // Set login creds
-  die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, 
-    "guest", "guest"), "Logging in");
+  if(!report_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, 
+    "guest", "guest"), "Logging in")){
+    return NULL;
+  }
 
   // Open channel 1 and check it went through
   amqp_channel_open(conn, 1);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+  if(!report_amqp_error(amqp_get_rpc_reply(conn), "Opening channel")){
+    return NULL;
+  }
 
   // Declare handshake queue
   amqp_queue_declare(conn, 1, queue_hand_name, 0, 0, 0, 0, amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring handshake queue");
+  if(!report_amqp_error(amqp_get_rpc_reply(conn), "Declaring handshake queue")){
+    return NULL;
+  }
 
   // Declare cmd queue
   amqp_queue_declare(conn, 1, queue_cmd_name, 0, 0, 0, 0, amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring cmd queue");
+  if(!report_amqp_error(amqp_get_rpc_reply(conn), "Declaring cmd queue")){
+    return NULL;
+  }
 
   // Declare value exchange
   amqp_exchange_declare(conn, 1, exch_value_name, exch_value_type, 0, 0, amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring value exchange");
+  if(!report_amqp_error(amqp_get_rpc_reply(conn), "Declaring value exchange")){
+    return NULL;
+  }
 
   // Declare messages exchange
   amqp_exchange_declare(conn, 1, exch_mess_name, exch_mess_type, 0, 0, amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring messages exchange");
+  if(!report_amqp_error(amqp_get_rpc_reply(conn), "Declaring messages exchange")){
+    return NULL;
+  }
 
   // Bind handshake queue to messages exchange
   amqp_queue_bind (conn, 1, queue_hand_name, exch_mess_name, route_hand_name, amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Bind handshake queue to messages exchange");
+  if(!report_amqp_error(amqp_get_rpc_reply(conn), "Bind handshake queue to messages exchange")){
+    return NULL;
+  }
 
   // Bind command queue to messages exchange
   amqp_queue_bind (conn, 1, queue_cmd_name, exch_mess_name, route_cmd_name, amqp_empty_table);
-  die_on_amqp_error(amqp_get_rpc_reply(conn), "Bind cmd queue to messages exchange");
+  if(!report_amqp_error(amqp_get_rpc_reply(conn), "Bind cmd queue to messages exchange")){
+    return NULL;
+  }
 
   return conn;
 }
 
+//TODO finish removing die on
 int perform_handshake(amqp_connection_state_t conn){
   ssize_t mes_len;
   char buf[BUF_LEN];
@@ -172,19 +207,6 @@ int perform_handshake(amqp_connection_state_t conn){
   die_on_error(amqp_basic_publish(conn, 1, exch_mess_name, amqp_empty_bytes, 0, 
     0, NULL, amqp_cstring_bytes(HANDSHAKE_STR)), "Publishing handshake");
   printf("publish message\n");
-
-  /* Original code - didn't seem to work correctly
-  die_on_amqp_error(amqp_basic_get(conn, 1, queue_hand_name, 1), "Get handshake.");
-  printf("get message\n");
-
-  if((mes_len = get_body(conn, buf, BUF_LEN)) < 0){
-    exit(1);
-  }
-  printf("get body\n");
-
-  //TODO act on message
-  printf("Handshake: %.*s\n", mes_len, buf);
-  */
 
   amqp_basic_consume(conn, 1, queue_hand_name, amqp_empty_bytes, 0, 1, 0, 
     amqp_empty_table);
@@ -258,4 +280,54 @@ ssize_t get_body(amqp_connection_state_t conn, char* buf, size_t buf_len){
   }
 
   return (ssize_t)body_size;
+}
+
+// Return 0 on error, 1 on no error
+int report_error(int x, char const *context) {
+  if (x < 0) {
+    char *errstr = amqp_error_string(-x);
+    fprintf(stderr, "%s: %s\n", context, errstr);
+    free(errstr);
+    return 0;
+  }
+
+  return 1;
+}
+
+// Return 0 on error, 1 on no error
+int report_amqp_error(amqp_rpc_reply_t x, char const *context) {
+  switch (x.reply_type) {
+    case AMQP_RESPONSE_NORMAL:
+      return 1;
+
+    case AMQP_RESPONSE_NONE:
+      fprintf(stderr, "%s: missing RPC reply type!\n", context);
+      break;
+
+    case AMQP_RESPONSE_LIBRARY_EXCEPTION:
+      fprintf(stderr, "%s: %s\n", context, amqp_error_string(x.library_error));
+      break;
+
+    case AMQP_RESPONSE_SERVER_EXCEPTION:
+      switch (x.reply.id) {
+        case AMQP_CONNECTION_CLOSE_METHOD: {
+          amqp_connection_close_t *m = (amqp_connection_close_t *) x.reply.decoded;
+          fprintf(stderr, "%s: server connection error %d, message: %.*s\n",
+            context, m->reply_code, (int) m->reply_text.len, (char *) m->reply_text.bytes);
+          break;
+        }
+        case AMQP_CHANNEL_CLOSE_METHOD: {
+          amqp_channel_close_t *m = (amqp_channel_close_t *) x.reply.decoded;
+          fprintf(stderr, "%s: server channel error %d, message: %.*s\n",
+          context, m->reply_code, (int) m->reply_text.len, (char *) m->reply_text.bytes);
+          break;
+        }
+        default:
+          fprintf(stderr, "%s: unknown server error, method id 0x%08X\n", context, x.reply.id);
+          break;
+      }
+      break;
+  }
+
+  return 0;
 }
